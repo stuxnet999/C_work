25f5ad72ccbd4beb57f6232cabf42fbde901896e
P. Abhiram Kumar <31406812+stuxnet999@users.noreply.github.com>
2018-01-23 15:25:38 +0000

    adding armstrong number checker

---
+++ c_challenges/armstrong.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a, b, c,d =0,e=0;
+	printf("enter the number:");
+	scanf("%d", &a);
+	b = a;
+	while (b!=0)
+    		{
+    			b = b/10;
+    			++e;
+    		}
+	b = a;
+	while (b!=0)
+    		{
+    			c = b%10;
+    			d = d+ pow(c,e);
+    			b = b/10;
+    		}
+	if (d==a)
+    		{
+    			printf("The no. is an armstrong number\n");
+    		}
+	else
+		{
+			printf("It isn't.\n");
+		}
+}
+
